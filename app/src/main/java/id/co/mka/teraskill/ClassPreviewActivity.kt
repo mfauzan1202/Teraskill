0a80177736a79cd6cbd7537d5e39d856b001864f
ArifshaReski <ifsharsky1205@gmail.com>
2022-10-17 17:11:15 +0800

    create class preview

---
+++ app/src/main/java/id/co/mka/teraskill/ClassPreviewActivity.kt
@@ -0,0 +1,11 @@
+package id.co.mka.teraskill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClassPreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_preview)
+    }
+}
