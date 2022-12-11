2082bdb9ab214b188e817ac18195e5460bd2be8d
ArifshaReski <ifsharsky1205@gmail.com>
2022-12-11 14:05:26 +0800

    create: webinars

---
+++ app/src/main/java/id/co/mka/teraskill/WebinarRegistrationActivity.kt
@@ -0,0 +1,11 @@
+package id.co.mka.teraskill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebinarRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webinar_registration)
+    }
+}
